6e109e75b0294ab73d9173eed741edeecd666c5c
Genry <genry.vyaznikov@gmail.com>
2019-04-06 12:55:00 +0300

    [Pods] All pods installed and workspace created

---
+++ Pods/Target Support Files/Pods-PostsViewer-PostsViewerTests/Pods-PostsViewer-PostsViewerTests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_PostsViewer_PostsViewerTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PostsViewer_PostsViewerTestsVersionString[];
+
